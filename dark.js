8957157ef13ffa0fc68421bbbd1acfcfc83862b7
Willyamdevon <151402518+Willyamdevon@users.noreply.github.com>
2023-11-18 23:24:03 +0300

    Add files via upload

---
+++ dark.js
@@ -0,0 +1,26 @@
+
+var dark = false;
+
+var a = document.body;
+var p = document.getElementById("select")
+
+
+function darkLight() {
+	
+	if (!dark) {
+		
+		a.className = "theme-dark";
+		
+		p.innerHTML = "Включить светлую тему";
+
+	} else {
+		
+		a.className = "theme-light";
+		
+		p.innerHTML = "Включить тёмную тему";
+	}
+
+	
+	dark = !dark;
+	
+}
